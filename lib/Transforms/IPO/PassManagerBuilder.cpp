1e5877aecb529634a5746352d4f6d444db66f2c0
Sébastien PELLÉ <sebastien.pelle@atos.net>
2018-06-15 10:11:26 +0200

    transfering obfuscation pass - sub, bcf, spli, fla, sobf

--- lib/Transforms/IPO/PassManagerBuilder.cpp
+++ lib/Transforms/IPO/PassManagerBuilder.cpp
@@ -38,6 +38,12 @@
 #include "llvm/Transforms/Scalar/GVN.h"
 #include "llvm/Transforms/Scalar/SimpleLoopUnswitch.h"
 #include "llvm/Transforms/Vectorize.h"
+#include "llvm/Transforms/Obfuscation/BogusControlFlow.h"
+#include "llvm/Transforms/Obfuscation/Flattening.h"
+#include "llvm/Transforms/Obfuscation/Split.h"
+#include "llvm/Transforms/Obfuscation/Substitution.h"
+#include "llvm/Transforms/Obfuscation/StringObfuscation.h"
+#include "llvm/CryptoUtils.h"
 
 using namespace llvm;
 
@@ -144,6 +150,33 @@ static cl::opt<bool> EnableGVNSink(
     "enable-gvn-sink", cl::init(false), cl::Hidden,
     cl::desc("Enable the GVN sinking pass (default = off)"));
 
+// Flags for Obfuscation
+static cl::opt<bool> Flattening(
+  "fla", cl::init(false),
+  cl::desc("Enable the flattening pass"));
+
+static cl::opt<bool> BogusControlFlow(
+  "bcf", cl::init(false),
+  cl::desc("Enable bogus control flow"));
+
+static cl::opt<bool> Substitution(
+  "sub", cl::init(false),
+  cl::desc("Enable instruction substitutions"));
+
+static cl::opt<std::string> AesSeed(
+  "aesSeed", cl::init(""),
+  cl::desc("seed for the AES-CTR PRNG"));
+
+static cl::opt<bool> Split(
+  "spli", cl::init(false),
+  cl::desc("Enable basic block splitting"));
+
+static cl::opt<std::string> Seed("seed", cl::init(""),
+                           cl::desc("seed for the random"));
+
+static cl::opt<bool> StringObf("sobf", cl::init(false),
+                           cl::desc("Enable the string obfuscation"));
+
 PassManagerBuilder::PassManagerBuilder() {
     OptLevel = 2;
     SizeLevel = 0;
@@ -165,6 +198,18 @@ PassManagerBuilder::PassManagerBuilder() {
     PrepareForThinLTO = EnablePrepareForThinLTO;
     PerformThinLTO = false;
     DivergentTarget = false;
+
+    // Initialization of the global cryptographically
+    // secure pseudo-random generator
+    if(!AesSeed.empty()) {
+        llvm::cryptoutils->prng_seed(AesSeed.c_str());
+        if(!llvm::cryptoutils->prng_seed(AesSeed.c_str()))
+          exit(1);
+    }
+
+    if(!Seed.empty()) {
+      llvm::cryptoutils->prng_seed(Seed.c_str());
+    }
 }
 
 PassManagerBuilder::~PassManagerBuilder() {
@@ -398,6 +443,11 @@ void PassManagerBuilder::populateModulePassManager(
   // Allow forcing function attributes as a debugging and tuning aid.
   MPM.add(createForceFunctionAttrsLegacyPass());
 
+  MPM.add(createSplitBasicBlock(Split));
+  MPM.add(createBogus(BogusControlFlow));
+  MPM.add(createFlattening(Flattening));
+  MPM.add(createStringObfuscation(StringObf));
+
   // If all optimizations are disabled, just run the always-inline pass and,
   // if enabled, the function merging pass.
   if (OptLevel == 0) {
@@ -425,6 +475,7 @@ void PassManagerBuilder::populateModulePassManager(
       MPM.add(createGlobalDCEPass());
     }
 
+    MPM.add(createSubstitution(Substitution));
     addExtensionsToPM(EP_EnabledOnOptLevel0, MPM);
 
     // Rename anon globals to be able to export them in the summary.
@@ -694,6 +745,7 @@ void PassManagerBuilder::populateModulePassManager(
   // resulted in single-entry-single-exit or empty blocks. Clean up the CFG.
   MPM.add(createCFGSimplificationPass());
 
+  MPM.add(createSubstitution(Substitution));
   addExtensionsToPM(EP_OptimizerLast, MPM);
 }
 
